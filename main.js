5acbce470b3eb59a7284c581f200c7bfc4e0b945
Radha <Radha4736@gmail.com>
2017-11-06 21:49:25 +0530

    Changes made

--- main.js
+++ main.js
@@ -15,6 +15,27 @@ Test.config(function ($routeProvider) {
 		templateUrl: 'pages/register.html',
 		controller: 'RegisterController'
 	})
+	.when('/cse',{
+		templateUrl: 'pages/cse.html',
+		controller: 'CseController'
+	})
+	.when('/civ',{
+		templateUrl: 'pages/civ.html',
+		controller: 'CivController'
+	})
+	.when('/ee',{
+		templateUrl: 'pages/ee.html',
+		controller: 'EeController'
+	})
+	.when('/Ece',{
+		templateUrl: 'pages/ece.html',
+		controller: 'EceController'
+	})
+	.when('/mech',{
+		templateUrl: 'pages/mech.html',
+		controller: 'MechController'
+	})
+
 })
 
 Test.controller('mainController', function($scope,$location){				//mainController
@@ -35,9 +56,39 @@ Test.controller('loginController',function($scope,$location) {						//loginContr
 			$location.url('register');
 		}
 })
-Test.controller('RegisterController',function($scope,$location) {						//loginController
+Test.controller('RegisterController',function($scope,$location) {				//RegisterController
 	$scope.goToRegister = function() {
 		 console.log('Go to home page')
 		$location.url('register');
+	}
+})
+Test.controller('CseController',function($scope,$location) {						//CseController
+	$scope.goToCse = function() {
+		 console.log('Go to cse page')
+		$location.url('cse');
+	}
+})
+Test.controller('CivController',function($scope,$location) {						//CivController
+	$scope.goToCiv = function() {
+		 console.log('Go to civil page')
+		$location.url('civ');
+	}
+})
+Test.controller('EceController',function($scope,$location) {						//EceController
+	$scope.goToEce = function() {
+		 console.log('Go to Ece page')
+		$location.url('ece');
+	}
+})
+Test.controller('EeController',function($scope,$location) {						//EeController
+	$scope.goToEe = function() {
+		 console.log('Go to Ee page')
+		$location.url('ee');
+	}
+})
+Test.controller('MechController',function($scope,$location) {						//MechController
+	$scope.goToMech = function() {
+		 console.log('Go to Mech page')
+		$location.url('mech');
 	}
 })
